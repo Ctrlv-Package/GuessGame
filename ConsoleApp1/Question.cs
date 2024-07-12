b94404ad93817a46690fc56cd7d9008b4601bbcf
Ctrlv-Package <pedrovenereo@yahoo.com>
2024-07-12 10:45:44 -0400

    Remove embedded repository and update .gitignore

--- ConsoleApp1/Question.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    public class Question
-    {
-        public string Hint { get; set; }
-        public string Answer { get; set; }
-        public string Category { get; set; }
-    }
-}
